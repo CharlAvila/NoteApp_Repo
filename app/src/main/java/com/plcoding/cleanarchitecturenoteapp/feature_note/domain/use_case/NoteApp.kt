59c581b1f62ad83333e5c64c5a39411163fd0518
CharlAvila <105625321+CharlAvila@users.noreply.github.com>
2022-06-08 13:43:31 +0800

    note app

---
+++ app/src/main/java/com/plcoding/cleanarchitecturenoteapp/feature_note/domain/use_case/NoteApp.kt
@@ -0,0 +1,7 @@
+package com.plcoding.cleanarchitecturenoteapp.feature_note.domain.use_case
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class NoteApp: Application()
